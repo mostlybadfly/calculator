d5803a3e63cc58b4663d13b8f300a0b653d01b8e
Daniel Nunez <reachdanielnunez@gmail.com>
2016-06-10 09:05:16 -0500

    Clean up some logic, working on performOp function

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', init, false);
-
-function init(){
+// no DOM logic before init.
+// include a stack to store numbers and ops
   function appendNum() {
+    // this is good for appending the number to the textfield.
+    // needs to add the number clicked to the stack as well
     if (clearOnInput==true) {
-      var result = document.getElementById("result");
       result.value = "";
     }
-    var result = document.getElementById("result");
     result.value = (result.value + this.innerHTML);
     clearOnInput = false;
   }
 
   function performOp() {
-    clearOnInput = false;
-    var result = document.getElementById("result");
-    result.value = (result.value + this.innerHTML);
+    // this will need to append an operation to the stack vs just the textfield.
+    // op buttons should be deactivated once one is pushed, until after a number
+    // checking below for value, if value push to stack along with op
+    if (result.value!='') {
+    alert(result.value);
+    } else {
+      alert("no value in textfield");
+    }
   }
 
   function evalResult() {
+    // needs to be rewriten so that num, op, num are being performed
+    // triggered when a second op or equals is clicked
     var result = document.getElementById("result");
     result.value = result.value ? eval(result.value) : '';
     clearOnInput = true;
@@ -26,8 +33,12 @@ function init(){
   function clearBox() {
     result = document.getElementById("result");
     result.value = "";
+    // needs logic to clear the current result
   }
-    
+
+function init(){
+  result = document.getElementById("result");
+
   var numButtons = document.getElementsByClassName("numbtn");
 
   for (var i = 0; i < numButtons.length; i++) {
@@ -44,7 +55,7 @@ function init(){
 
   equalButton.addEventListener("click", evalResult, false);
 
-  var clearOnInput = false;
+  clearOnInput = false;
 
   var clearButton = document.getElementById("clearbtn");
 
